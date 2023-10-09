cb36f1c1f8c0beca5f22ff025081f77e52337663
Amy Kleynhans <amykleynhans@yahoo.com>
2023-10-09 11:01:01 -0600

    populated page for post route

---
+++ controllers/api/drinkRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const { Drink } = require('../../models');
+// const withAuth = require('../../utils/auth');
+
+router.post('/saveddrinks', async (req, res) => {
+  try {
+    const newDrink = await Drink.create({
+        ...req.body,
+    
+    });
+
+    res.status(200).json(newDrink);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
