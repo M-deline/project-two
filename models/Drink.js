3310808345f8d54d7f075ed88c23e19be1a95d50
Amy Kleynhans <amykleynhans@yahoo.com>
2023-10-09 11:07:33 -0600

    added rows for Drink

---
+++ models/Drink.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Drink extends Model {}
+
+Drink.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    steps: {
+      type: DataTypes.STRING,
+      allowNull: false,
+
+   
+    },
+
+  },
+  {
+
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'drink',
+  }
+);
+
+module.exports = Drink;
