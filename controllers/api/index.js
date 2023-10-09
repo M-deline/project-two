2ef70c18e448373628f72abda421940408bde191
Amy Kleynhans <amykleynhans@yahoo.com>
2023-10-09 11:44:38 -0600

    added user routes to index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const drinkRoutes = require('./drinkRoutes');
+const userRoutes = require('./userRoutes');
 
 router.use('/drink', drinkRoutes);
+router.use('/login', userRoutes);
 
 module.exports = router;
