f19d0d9fc7024862ab151e11a2fb593ce67d4a19
Amy Kleynhans <amykleynhans@yahoo.com>
2023-10-09 11:54:59 -0600

    populated user json and added route to seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,10 +1,16 @@
 const sequelize = require('../config/connection');
-const { Drink } = require('../models');
+const { User, Drink } = require('../models');
 
+const userData = require('./userData.json');
 const drinkData = require('./drinkData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
 
   for (const drink of drinkData) {
 
